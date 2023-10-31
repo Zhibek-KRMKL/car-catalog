34a0febb680c24a05e2ee83427279db2cb41c9ad
Zhibek <thezhibeks@gmail.com>
2023-10-31 13:24:09 +0600

    added new component home with file Home.jsx and delete App.jsx

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,23 @@
+
+
+function Home() {
+
+
+    return (
+      <>
+        <div>
+          <h1>Cars catalog</h1>
+          <div>
+            <div>
+              <h2>Car 1</h2>
+              <p>$100 000</p>
+              <button>Read more</button>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Home
+  
